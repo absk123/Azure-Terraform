74c197f40f6b443833dd2d782ba86a52ba666f8d
Abhisek <absk.sing@outllok.com>
2024-02-12 23:25:09 +0530

    ADDED NEW FEATURE

--- 6_FileStructure/provider.tf
+++ 6_FileStructure/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.91.0"
     }
   }
+  
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "tfstatefile.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
